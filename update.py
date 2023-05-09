0850332674adbcb57c04fed18ddc99ddf074f48b
Frankline Misango Oyolo <95913228+FranklineMisango@users.noreply.github.com>
2023-05-09 14:37:24 +0800

    update the rendering function

---
+++ update.py
@@ -0,0 +1,49 @@
+#Import the necessary packages
+#Demo
+import json
+import os
+import random
+
+import cv2 as cv
+import keras.backend as K
+import numpy as np
+import scipy.io
+
+
+if __name__ == '__main__':
+    img_width, img_height = 224, 224
+    model = load_model()
+    model.load_weights('/content/models/model.15-0.89.hdf5')
+
+    cars_meta = scipy.io.loadmat('devkit/cars_meta')
+    class_names = cars_meta['class_names']  # shape=(1, 196)
+    class_names = np.transpose(class_names)
+
+    test_path = 'data/test/'
+    test_images = [f for f in os.listdir(test_path) if
+                   os.path.isfile(os.path.join(test_path, f)) and f.endswith('.jpg')]
+
+    num_samples = 20
+    samples = random.sample(test_images, num_samples)
+    results = []
+    for i, image_name in enumerate(samples):
+        filename = os.path.join(test_path, image_name)
+        print('Start processing image: {}'.format(filename))
+        bgr_img = cv.imread(filename)
+        bgr_img = cv.resize(bgr_img, (img_width, img_height), cv.INTER_CUBIC)
+        rgb_img = cv.cvtColor(bgr_img, cv.COLOR_BGR2RGB)
+        rgb_img = np.expand_dims(rgb_img, 0)
+        preds = model.predict(rgb_img)
+        prob = np.max(preds)
+        class_id = np.argmax(preds)
+        text = ('Predict: {}, prob: {:.4}'.format(class_names[class_id][0][0], prob))
+        results.append({'label': class_names[class_id][0][0], 'prob': '{:.4}'.format(prob)})
+        # add predicted text to the image
+        cv.putText(bgr_img, text, (10, 30), cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2, cv.LINE_AA)
+        cv.imwrite('/content/images/{}_out.png'.format(i), bgr_img)
+
+    print(results)
+    with open('results.json', 'w') as file:
+        json.dump(results, file, indent=4)
+
+    K.clear_session()
